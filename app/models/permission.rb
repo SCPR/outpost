1b6213cb9e576d78d873c1427d88e84ce7aab5f5
Bryan Ricker <bricker88@gmail.com>
2013-03-14 00:39:12 -0700

    Move Permission into Outpost

---
+++ app/models/permission.rb
@@ -0,0 +1,19 @@
+##
+# Permission
+#
+class Permission < ActiveRecord::Base
+  #-------------------
+  # Association
+  has_many :user_permissions
+  has_many :users, through: :user_permissions, dependent: :destroy
+  
+  #-------------------
+  # Validation
+  validates :resource, uniqueness: true
+  
+  #-------------------
+  
+  def title
+    self.resource.titleize
+  end
+end
