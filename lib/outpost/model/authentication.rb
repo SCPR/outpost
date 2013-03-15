98fc8a2c97c29a2c06208174a1fb401a1b15c403
Bryan Ricker <bricker88@gmail.com>
2013-03-14 23:26:30 -0700

    Revert "Remove username generation; use e-mail for login"
    This reverts commit d816d7c5a1361870fe9168a1ebe2446b40c65515.
    
    Conflicts:
    	lib/outpost/model/authentication.rb
    	spec/lib/model/authentication_spec.rb

--- lib/outpost/model/authentication.rb
+++ lib/outpost/model/authentication.rb
@@ -7,20 +7,42 @@ module Outpost
         has_secure_password
 
         before_validation :downcase_email, if: -> { self.email_changed? }
+        before_validation :generate_username, on: :create, if: -> { self.username.blank? }
 
         validates :name, presence: true
-        validates :email, presence: true, uniqueness: true
+        validates :email, presence: true
+        validates :username, presence: true, uniqueness: true
       end
 
       module ClassMethods
-        def authenticate(email, unencrypted_password)
-          self.find_by_email(email).try(:authenticate, unencrypted_password)
+        def authenticate(username, unencrypted_password)
+          self.find_by_username(username).try(:authenticate, unencrypted_password)
         end
       end
 
-
       private
 
+      # Private: Generate a username based on real name
+      #
+      # Returns String of the username
+      def generate_username
+        names       = self.name.to_s.split
+        base        = (names.first.chars.first + names.last).downcase.gsub(/\W/, "")
+        dirty_name  = base
+
+        i = 1
+        while self.class.exists?(username: dirty_name)
+          dirty_name = base + i.to_s
+          i += 1
+        end
+
+        self.username = dirty_name
+      end
+
+      # Private: Downcase the user's e-mail
+      #
+      # Returns String of the e-mail
+>>>>>>> parent of d816d7c... Remove username generation; use e-mail for login
       def downcase_email
         if self.email.present?
           self.email = self.email.downcase
