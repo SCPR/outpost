a170eeae1766b08dccf128357484aa540bcf2c6f
Bryan Ricker <bricker88@gmail.com>
2013-03-17 00:23:11 -0700

    Put authorize_resource in the Authorization module

--- lib/outpost/controller/authorization.rb
+++ lib/outpost/controller/authorization.rb
@@ -6,17 +6,21 @@ module Outpost
   module Controller
     module Authorization
       # Make sure the user can authorize the current resource
-      def authorize!(resource=nil)
-        resource ||= Outpost::Helpers::Naming.to_class(params[:controller])
-        
+      def authorize(resource)
         if !current_user.can_manage?(resource)
           handle_unauthorized(resource)
         end
       end
 
+      # Use this for before_filter. Should be overridden for custom behavor.
+      def authorize_resource
+        authorize(self.class.model)
+      end
+
       # What to do when a user doesn't have proper permissions
       def handle_unauthorized(resource)
-        redirect_to outpost_root_path, alert: "You don't have permission to manage #{resource.to_title.pluralize}"
+        redirect_to outpost_root_path, 
+          alert: "You don't have permission to manage #{resource.to_title.pluralize}"
         return false
       end
     end # Authorization
