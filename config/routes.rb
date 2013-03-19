9b0ce8235329f3f63d29000812ba764c8a4c00b0
Bryan Ricker <bricker88@gmail.com>
2013-03-19 02:29:16 -0700

    Remove root from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :outpost do
-    root to: 'home#index'
-    
+  namespace :outpost do    
     resources :sessions, only: [:create, :destroy]
     get 'login'  => "sessions#new", as: :login
     get 'logout' => "sessions#destroy", as: :logout
