8a545a3534f1ac6ab335a9726058bc75811cd232
Bryan Ricker <bricker88@gmail.com>
2013-03-19 03:13:58 -0700

    Move routes out of outpost for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  namespace :outpost do    
-    resources :sessions, only: [:create, :destroy]
-    get 'login'  => "sessions#new", as: :login
-    get 'logout' => "sessions#destroy", as: :logout
-  end
+
 end
--- spec/internal/config/routes.rb
+++ spec/internal/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   namespace :outpost do
     root to: 'home#index'
+    resources :sessions, only: [:create, :destroy]
+    get 'login'  => "sessions#new", as: :login
+    get 'logout' => "sessions#destroy", as: :logout
   end
 end
