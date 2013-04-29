183fa0a50e686855f4e9a86015f7d2ec52bd73b1
Bryan Ricker <bricker88@gmail.com>
2013-04-29 13:58:31 -0700

    Support Rails 4 UnknownFormat in CustomErrors

--- lib/outpost/controller/custom_errors.rb
+++ lib/outpost/controller/custom_errors.rb
@@ -3,12 +3,21 @@ module Outpost
     module CustomErrors
       extend ActiveSupport::Concern
       
+      NOT_FOUND_ERROR_CLASSES = [
+        ActionController::RoutingError, 
+        ActionView::MissingTemplate, 
+        ActionController::UnknownController, 
+        AbstractController::ActionNotFound, 
+        ActiveRecord::RecordNotFound
+      ]
+
+      if defined?(ActionController::UnknownFormat)
+        NOT_FOUND_ERROR_CLASSES << ActionController::UnknownFormat
+      end
+
       included do
         rescue_from StandardError, with: ->(e) { render_error(500, e) }
-        
-        rescue_from ActionController::RoutingError, ActionView::MissingTemplate, 
-          ActionController::UnknownController, ::AbstractController::ActionNotFound, 
-          ActiveRecord::RecordNotFound, with: ->(e) { render_error(404, e) }
+        rescue_from *NOT_FOUND_ERROR_CLASSES, with: ->(e) { render_error(404, e) }
       end
       
       #----------------------
