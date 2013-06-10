a766ff1e5996cfa3cf098d3a1c4b0e17c5d5b9b8
Bryan Ricker <bricker88@gmail.com>
2013-06-10 16:59:55 -0700

    Fix deprecation warning

--- lib/outpost/model/routing.rb
+++ lib/outpost/model/routing.rb
@@ -108,8 +108,8 @@ module Outpost
       
 
       # http://scpr.org/blogs/2012/...
-        if path = self.link_path(options)
       def public_url(options={})
+        if path = self.public_path(options)
           File.join(
             "http://#{Rails.application.default_url_options[:host]}", path)
         end
