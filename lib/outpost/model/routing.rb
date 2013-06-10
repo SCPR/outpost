0b3c6668f1198a5ca0e2810a20488206bdf62684
Bryan Ricker <bricker88@gmail.com>
2013-06-10 16:12:43 -0700

    Add *_url methods to routing

--- lib/outpost/model/routing.rb
+++ lib/outpost/model/routing.rb
@@ -17,26 +17,50 @@ module Outpost
             "new_outpost_#{self.singular_route_key}_path")
         end
         
+        # http://kpcc.org/outpost/blog_entries/new
+        def admin_new_url
+          @admin_new_url ||= Rails.application.routes.url_helpers.send(
+            "new_outpost_#{self.singular_route_key}_url")
+        end
+
+
         # /outpost/blog_entries
         def admin_index_path
           @admin_index_path ||= Rails.application.routes.url_helpers.send(
             "outpost_#{self.route_key}_path")
         end
+
+        # http://kpcc.org/outpost/blog_entries
+        def admin_index_url
+          @admin_index_url ||= Rails.application.routes.url_helpers.send(
+            "outpost_#{self.route_key}_url")
         end
       end
+
 
       # /outpost/blog_entries/20/edit
       def admin_edit_path
         @admin_edit_path ||= Rails.application.routes.url_helpers.send(
           "edit_outpost_#{self.class.singular_route_key}_path", self.id)
       end
+
+      # http://kpcc.org/outpost/blog_entries/20/edit
+      def admin_edit_url
+        @admin_edit_url ||= Rails.application.routes.url_helpers.send(
+          "edit_outpost_#{self.class.singular_route_key}_url", self.id)
       end
+
 
       # /outpost/blog_entries/20
       def admin_show_path
         @admin_show_path ||= Rails.application.routes.url_helpers.send(
           "outpost_#{self.class.singular_route_key}_path", self.id)
       end
+
+      # http://kpcc.org/outpost/blog_entries/20
+      def admin_show_url
+        @admin_show_url ||= Rails.application.routes.url_helpers.send(
+          "outpost_#{self.class.singular_route_key}_url", self.id)
       end
       
       # Uses self.class::ROUTE_KEY to generate
