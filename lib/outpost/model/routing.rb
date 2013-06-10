f33fb8418a8adaa93e3deb18f80b45ecf363ebf8
Bryan Ricker <bricker88@gmail.com>
2013-06-10 16:19:06 -0700

    Add aliases for non-GET routes.

--- lib/outpost/model/routing.rb
+++ lib/outpost/model/routing.rb
@@ -7,6 +7,17 @@ module Outpost
     #
     # These methods are just delegations to your app's routes.
     #
+    # Provides alias methods for non-GET routes:
+    #
+    # * admin_index_path => admin_create_path
+    # * admin_show_path  => admin_update_path, admin_delete_path
+    #
+    # So you can do, for example:
+    #
+    #   POST admin_create_path
+    #
+    # Which would make more sense than `POST admin_index_path`,
+    # even though they are the same path.
     module Routing
       extend ActiveSupport::Concern
       
@@ -29,12 +40,17 @@ module Outpost
           @admin_index_path ||= Rails.application.routes.url_helpers.send(
             "outpost_#{self.route_key}_path")
         end
+        
+        alias_method :admin_create_path, :admin_index_path
 
         # http://kpcc.org/outpost/blog_entries
         def admin_index_url
           @admin_index_url ||= Rails.application.routes.url_helpers.send(
             "outpost_#{self.route_key}_url")
         end
+
+        alias_method :admin_create_url, :admin_index_url
+
       end
 
 
@@ -57,11 +73,18 @@ module Outpost
           "outpost_#{self.class.singular_route_key}_path", self.id)
       end
 
+      alias_method :admin_update_path, :admin_show_path
+      alias_method :admin_delete_path, :admin_show_path
+
       # http://kpcc.org/outpost/blog_entries/20
       def admin_show_url
         @admin_show_url ||= Rails.application.routes.url_helpers.send(
           "outpost_#{self.class.singular_route_key}_url", self.id)
       end
+
+      alias_method :admin_update_url, :admin_show_url
+      alias_method :admin_delete_url, :admin_show_url
+
       
       # Uses self.class::ROUTE_KEY to generate
       # the front-end path to this object
