1e15e34dfa9a0d297b8d6bf72debfe696daccb8b
Bryan Ricker <bricker88@gmail.com>
2013-06-10 17:00:41 -0700

    Specs for the new routing methods

--- spec/lib/model/routing_spec.rb
+++ spec/lib/model/routing_spec.rb
@@ -8,6 +8,14 @@ describe Outpost::Model::Routing do
       Person.admin_new_path
     end
   end
+  
+  describe '::admin_new_url' do
+    it 'uses the apps routes' do
+      Person.stub(:singular_route_key) { "employee" }
+      Rails.application.routes.url_helpers.should_receive("new_outpost_employee_url")
+      Person.admin_new_url
+    end
+  end
 
   #---------------------
   
@@ -18,71 +26,130 @@ describe Outpost::Model::Routing do
       Person.admin_index_path
     end
   end
-  
-  #---------------------
-  
-  describe "#admin_edit_path" do
-    it "figures out the edit path using singular_route_key and the record's id" do
-      Person.stub(:singular_route_key) { "employee" }
-      person = create :person, name: "Thelonious Monk"
-      Rails.application.routes.url_helpers.should_receive("edit_outpost_employee_path").with(person.id)
-      person.admin_edit_path
+
+  describe '::admin_index_url' do
+    it 'uses the apps routes' do
+      Person.stub(:route_key) { "employees" }
+      Rails.application.routes.url_helpers.should_receive("outpost_employees_url")
+      Person.admin_index_url
     end
   end
 
+  describe '::admin_create_path' do
+    it 'is admin_index_path' do
+      Person.admin_create_path.should eq Person.admin_index_path
+    end
+  end
+
+  describe '::admin_create_url' do
+    it 'is admin_index_url' do
+      Person.admin_create_url.should eq Person.admin_index_url
+    end
+  end
+  
   #---------------------
   
-  describe "#admin_show_path" do
-    it "figures out the show path using singular_route_key and the record's id" do
-      Person.stub(:singular_route_key) { "employee" }
-      person = create :person, name: "Charles Mingus"
-      Rails.application.routes.url_helpers.should_receive("outpost_employee_path").with(person.id)
-      person.admin_show_path
+  context 'member routes' do
+    let(:person) { create :person, name: "Thelonious Monk" }
+
+    describe "#admin_edit_path" do
+      it "figures out the edit path using singular_route_key and the record's id" do
+        Person.stub(:singular_route_key) { "employee" }
+        Rails.application.routes.url_helpers.should_receive("edit_outpost_employee_path").with(person.id)
+        person.admin_edit_path
+      end
     end
-  end
-  
-  #----------------
-  
-  describe "#link_path" do
-    let(:person) { create :person, name: "Bob Loblaw" }
+
+    describe "#admin_edit_url" do
+      it "uses the apps routes" do
+        Person.stub(:singular_route_key) { "employee" }
+        Rails.application.routes.url_helpers.should_receive("edit_outpost_employee_url").with(person.id)
+        person.admin_edit_url
+      end
+    end
+
+    #---------------------
     
-    it "returns nil if #route_hash is blank" do
-      person.stub(:route_hash) { Hash.new }
-      person.link_path.should eq nil
+    describe "#admin_show_path" do
+      it "figures out the show path using singular_route_key and the record's id" do
+        Person.stub(:singular_route_key) { "employee" }
+        Rails.application.routes.url_helpers.should_receive("outpost_employee_path").with(person.id)
+        person.admin_show_path
+      end
+    end
+
+    describe "#admin_show_url" do
+      it "uses the apps routes" do
+        Person.stub(:singular_route_key) { "employee" }
+        Rails.application.routes.url_helpers.should_receive("outpost_employee_url").with(person.id)
+        person.admin_show_url
+      end
+    end
+
+    describe '#admin_update_path' do
+      it 'is admin_show_path' do
+        person.admin_update_path.should eq person.admin_show_path
+      end
+    end
+
+    describe '#admin_update_url' do
+      it 'is admin_show_url' do
+        person.admin_update_url.should eq person.admin_show_url
+
+      end
+    end
+
+    describe '#admin_destroy_path' do
+      it 'is admin_show_path' do
+        person.admin_destroy_path.should eq person.admin_show_path
+      end
+    end
+
+    describe '#admin_destroy_path' do
+      it 'is admin_show_url' do
+        person.admin_destroy_url.should eq person.admin_show_url
+      end
     end
     
-    it "returns nil if if ROUTE_KEY isn't defined" do
-      pidgeon = Pidgeon.new
-      pidgeon.stub(:route_hash) { { id: 1, slug: "cool-dude" } }
-      pidgeon.link_path.should eq nil
+    #----------------
+    
+    describe "#public_path" do
+      it "returns nil if #route_hash is blank" do
+        person.stub(:route_hash) { Hash.new }
+        person.public_path.should eq nil
+      end
+      
+      it "returns nil if if ROUTE_KEY isn't defined" do
+        pidgeon = Pidgeon.new
+        pidgeon.stub(:route_hash) { { id: 1, slug: "cool-dude" } }
+        pidgeon.public_path.should eq nil
+      end
+      
+      it "returns the route helper with the route hash" do
+        Rails.application.routes.url_helpers.should_receive(:person_path).with(person.route_hash).and_return("/employee/999")
+        person.public_path.should eq "/employee/999"
+      end
     end
     
-    it "returns the route helper with the route hash" do
-      Rails.application.routes.url_helpers.should_receive(:person_path).with(person.route_hash).and_return("/employee/999")
-      person.link_path.should eq "/employee/999"
-    end
-  end
-  
-  #----------------
-  
-  describe "#route_hash" do
-    it "is just an empty hash, meant to be overridden" do
-      Pidgeon.new.route_hash.should eq Hash.new
-    end
-  end
-  
-  #----------------
-  
-  describe "#remote_link_path" do
-    let(:person) { create :person, name: "Roy Haynes" }
+    #----------------
     
-    before :each do
-      person.stub(:link_path) { "/people/linkpath" }
-      Rails.application.stub(:default_url_options).and_return(host: "www.omg.com")
+    describe "#route_hash" do
+      it "is just an empty hash, meant to be overridden" do
+        Pidgeon.new.route_hash.should eq Hash.new
+      end
     end
     
-    it "contains the object's link path and configured remote URL" do
-      person.remote_link_path.should eq "http://www.omg.com/people/linkpath"
+    #----------------
+    
+    describe "#public_url" do
+      before :each do
+        person.stub(:public_path) { "/people/linkpath" }
+        Rails.application.stub(:default_url_options).and_return(host: "www.omg.com")
+      end
+      
+      it "contains the object's link path and configured remote URL" do
+        person.public_url.should eq "http://www.omg.com/people/linkpath"
+      end
     end
   end
 end
